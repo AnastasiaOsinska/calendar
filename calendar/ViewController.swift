b7104a80c971028dcc611a4044066d4b6ea5922f
Anastasia Osinska <60783750+AnastasiaOsinska@users.noreply.github.com>
2020-07-01 17:47:52 +0300

    Initial Commit

---
+++ calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calendar
+//
+//  Created by Anastasiya Osinskaya on 7/1/20.
+//  Copyright © 2020 Anastasiya Osinskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
